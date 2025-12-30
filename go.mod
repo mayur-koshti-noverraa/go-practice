7c216053711d4348900346c1983bbdf02dd6fae2
mayur.koshti.noverraa <mayur.koshti.noverraa@gmail.com>
2025-12-30 17:03:16 +0530

    Added Dependancy &  Q&A file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module myapp
 
 go 1.25.5
+
+require golang.org/x/text v0.32.0 // indirect
--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	c := cases.Title(language.English)
+	fmt.Println(c.String("Hello Go Module"))
+
 }
